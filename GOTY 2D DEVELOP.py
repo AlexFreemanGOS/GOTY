513f39d55e1ae76f2c5cedc7c5a435c1912e5b93
totktokokos <45895868+totktokokos@users.noreply.github.com>
2018-12-15 14:49:59 +0300

    Update GOTY 2D DEVELOP.py

--- GOTY 2D DEVELOP.py
+++ GOTY 2D DEVELOP.py
@@ -5,8 +5,8 @@ w = root.winfo_screenwidth()
 h = root.winfo_screenheight() 
 w = w//2 
 h = h//2 
-w = w - 512
-h = h - 300
+w = w - 700
+h = h - 450
 root.geometry('"1366x768"+{}+{}'.format(w, h))
 root.resizable(FALSE,FALSE)
 
