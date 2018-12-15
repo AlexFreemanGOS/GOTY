b3e8e037d612a1f9c9f936a213aac93e1f0068a8
AlexFreemanGOS <45854704+AlexFreemanGOS@users.noreply.github.com>
2018-12-15 16:08:47 +0300

    Update GOTY 2D DEVELOP.py

--- GOTY 2D DEVELOP.py
+++ GOTY 2D DEVELOP.py
@@ -9,12 +9,19 @@ w = w - 700
 h = h - 450
 root.geometry('1366x768+{}+{}'.format(w, h))
 root.resizable(FALSE,FALSE)
-
-
-rootimg=PhotoImage(file="root.gif")
-
+rootimg=PhotoImage(file="GOTYLABEL.png")
 l = Label(image = rootimg)
 l.pack()
-  
+
+bstartimg=PhotoImage(file="start.png")
+bstart = Button(root, image = bstartimg)
+bsettingsimg=PhotoImage(file="settings.png")
+bsettings = Button(root, image = bsettingsimg)
+bexitimg=PhotoImage(file="exit.png")
+bexit = Button(root, image = bexitimg)
+
+bstart.place(x=463, y=220)
+bsettings.place(x=463, y=370)
+bexit.place(x=463, y=520)
+
 root.mainloop()
-#sanyasosatisaiaisa
