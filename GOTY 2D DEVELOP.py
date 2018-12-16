8a00bbcdc330e9e4bc95e59b48a41eee4e94b67f
totktokokos <45895868+totktokokos@users.noreply.github.com>
2018-12-16 15:14:47 +0300

    Update GOTY 2D DEVELOP.py

--- GOTY 2D DEVELOP.py
+++ GOTY 2D DEVELOP.py
@@ -11,7 +11,14 @@ root.geometry('1366x768+{}+{}'.format(w, h))
 root.resizable(FALSE,FALSE)
 rootimg=PhotoImage(file="GOTYLABEL.png")
 
-l1=Label(bg='red')
+l1=Label(bg='lightgreen',text='СУПЕР КРУТАЯ\nИСТОРИЯ',font='Courier 48 bold')
+nextimg=PhotoImage(file='next.png')
+bnext=Button(image=nextimg)
+shopimg=PhotoImage(file='shop.png')
+bshop=Button(image=shopimg)
+
+
+
 
 def start(event):
     
@@ -19,17 +26,26 @@ def start(event):
     bsettings.destroy()
     bexit.destroy()
     l.destroy()
+    l1.pack(expand=1,fill=BOTH)
+    def nextt(event):
+        l1.destroy()
+        bnext.destroy()
+        
+        bshop.place(x=10,y=200)
+    bnext.bind('<Button-1>',nextt)  
+    bnext.place(x=1100,y=700)
     
-    #GAME    
     
-    l1.pack()
-
-
+    
+def upgrade(event):
+    print('sasa')
 
 
 
 
 l = Label(image = rootimg)
+l.pack()
+
 bstartimg=PhotoImage(file="start.png")
 bstart = Button(root, image = bstartimg)
 bsettingsimg=PhotoImage(file="settings.png")
@@ -47,16 +63,5 @@ bstart.bind('<Button-1>',start)
 bstart.place(x=463, y=220)
 bsettings.place(x=463, y=370)
 bexit.place(x=463, y=520)
-l.pack()
 root.mainloop()
-
-bstartimg=PhotoImage(file="start.png")
-bstart = Button(root, image = bstartimg)
-bsettingsimg=PhotoImage(file="settings.png")
-bsettings = Button(root, image = bsettingsimg)
-bexitimg=PhotoImage(file="exit.png")
-bexit = Button(root, image = bexitimg)
-
-
-
 
